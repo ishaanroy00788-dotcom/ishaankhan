edff441711d394ac520b0eb9b0b901c27c40cbdb
ishaanroy00788-dotcom <ishaanroy00788@gmail.com>
2026-06-10 15:06:26 +0530

    not need to change

---
+++ script.js
@@ -0,0 +1,123 @@
+const quiz = [
+
+{
+question:"What is the capital of India?",
+options:["Delhi","Mumbai","Jaipur","Chennai"],
+answer:0
+},
+
+{
+question:"Which language is used for web pages?",
+options:["Python","HTML","Java","C++"],
+answer:1
+},
+
+{
+question:"2 + 2 = ?",
+options:["3","5","4","6"],
+answer:2
+},
+
+{
+question:"Who developed Python?",
+options:["Guido van Rossum","Bill Gates","Elon Musk","Mark Zuckerberg"],
+answer:0
+},
+
+{
+question:"Which planet is called the Red Planet?",
+options:["Earth","Mars","Jupiter","Venus"],
+answer:1
+},
+
+{
+question:"What does CPU stand for?",
+options:["Central Process Unit","Central Processing Unit","Computer Processing Unit","Core Processing Unit"],
+answer:1
+},
+
+{
+question:"Which is the largest ocean?",
+options:["Atlantic","Indian","Pacific","Arctic"],
+answer:2
+},
+
+{
+question:"Python is a?",
+options:["Snake","Programming Language","Game","Browser"],
+answer:1
+},
+
+{
+question:"HTML stands for?",
+options:["Hyper Text Markup Language","High Text Machine Language","Hyper Transfer Markup Language","Home Tool Markup Language"],
+answer:0
+},
+
+{
+question:"CSS is used for?",
+options:["Database","Styling","Gaming","Networking"],
+answer:1
+},
+
+{
+question:"JavaScript runs in?",
+options:["Browser","Printer","Monitor","Scanner"],
+answer:0
+},
+
+{
+question:"Which company created Windows?",
+options:["Google","Apple","Microsoft","Intel"],
+answer:2
+},
+
+{
+question:"5 x 6 = ?",
+options:["25","30","35","40"],
+answer:1
+},
+
+{
+question:"Which is the national animal of India?",
+options:["Lion","Tiger","Elephant","Leopard"],
+answer:1
+},
+
+{
+question:"What is the capital of Rajasthan?",
+options:["Jodhpur","Udaipur","Jaipur","Kota"],
+answer:2
+},
+
+{
+question:"Which tag creates a heading in HTML?",
+options:["<p>","<h1>","<div>","<span>"],
+answer:1
+},
+
+{
+question:"Which data type stores text in Python?",
+options:["int","float","str","bool"],
+answer:2
+},
+
+{
+question:"Which symbol is used for comments in Python?",
+options:["//","#","<!-- -->","**"],
+answer:1
+},
+
+{
+question:"Which is the fastest land animal?",
+options:["Tiger","Horse","Cheetah","Leopard"],
+answer:2
+},
+
+{
+question:"How many continents are there?",
+options:["5","6","7","8"],
+answer:2
+}
+
+];
